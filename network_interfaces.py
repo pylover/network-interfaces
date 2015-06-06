9968a9c5675abc59be6ca7c567f2deac1eaf3b5c
pylover <vahid.mardani@gmail.com>
2015-06-06 06:00:52 +0430

    writing unittests

---
+++ network_interfaces.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+import re
+import os
+__author__ = 'vahid'
+
+
+class Stanza(object):
+    type = None
+    _filename = None
+
+    def __init__(self, filename, *headers):
+        self._filename = filename
+        self._headers = headers
+        self._items = []
+
+    def add_entry(self, l):
+        cells = re.split('\s+', l)
+        if cells:
+            self._items.append(cells)
+
+    def __repr__(self):
+        return ' '.join(self._headers)
+
+    def __getattr__(self, item):
+        key = item.replace('_', '-')
+        try:
+            return ' '.join([i for i in self._items if i[0] == key][0][1:])
+        except IndexError:
+            raise AttributeError('%s %s' % (object.__repr__(self), item))
+
+    @classmethod
+    def is_stanza(cls, s):
+        return re.match(r'^(iface|mapping|auto|allow-|source).*', s)
+
+    @classmethod
+    def subclasses(cls):
+        return cls.__subclasses__() + [g for s in cls.__subclasses__()
+                                       for g in s.subclasses()]
+
+    @classmethod
+    def create(cls, header, filename):
+        cells = re.split('\s+', header)
+        stanza_type = cells[0]
+        subclasses = cls.subclasses()
+
+        # Checking for exact match
+        for subclass in subclasses:
+            if subclass.type and stanza_type == subclass.type:
+                return subclass(filename, *cells)
+
+        # Partial start match
+        for subclass in subclasses:
+            if subclass.type and stanza_type.startswith(subclass.type):
+                return subclass(filename, *cells)
+
+
+class MultilineStanza(Stanza):
+
+    def __repr__(self):
+        return '%s\n%s\n' % (
+            super(MultilineStanza, self).__repr__(),
+            '\n'.join(['  ' + ' '.join(i) for i in self._items]))
+
+
+class StartupStanza(Stanza):
+
+    @property
+    def mode(self):
+        return self._headers[0]
+
+    @property
+    def iface_name(self):
+        return self._headers[1]
+
+
+class Auto(StartupStanza):
+    type = 'auto'
+
+
+class Allow(StartupStanza):
+    type = 'allow-'
+
+
+class Iface(MultilineStanza):
+    type = 'iface'
+    startup = None
+
+    @property
+    def name(self):
+        return self._headers[1]
+
+    @name.setter
+    def name(self, val):
+        self._headers[1] = val
+
+    @property
+    def address_family(self):
+        return self._headers[2]
+
+    @address_family.setter
+    def address_family(self, val):
+        self._headers[2] = val
+
+    @property
+    def method(self):
+        return self._headers[3]
+
+    @method.setter
+    def method(self, val):
+        self._headers[3] = val
+
+    def __repr__(self):
+        return '%s\n%s' % (self.startup, super(Iface, self).__repr__())
+
+
+class Mapping(MultilineStanza):
+    type = 'mapping'
+
+
+class Source(Stanza):
+    type = 'source'
+
+    @property
+    def source_filename(self):
+        return self._headers[1]
+
+    @source_filename.setter
+    def source_filename(self, val):
+        self._headers[1] = val
+
+
+class SourceDirectory(Stanza):
+    type = 'source-directory'
+
+    @property
+    def source_directory(self):
+        return self._headers[1]
+
+    @source_directory.setter
+    def source_directory(self, val):
+        self._headers[1] = val
+
+
+class InterfacesFile(object):
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.dirname = os.path.dirname(filename)
+        self.sub_files = []
+        current_stanza = None
+        stanzas = []
+        with open(self.filename) as f:
+            for l in f:
+                line = l.strip()
+                if not line or line.startswith('#'):
+                    continue
+                if Stanza.is_stanza(line):
+                    if current_stanza:
+                        stanzas.append(current_stanza)
+                    current_stanza = Stanza.create(line, self.filename)
+                else:
+                    current_stanza.add_entry(line)
+            if current_stanza:
+                stanzas.append(current_stanza)
+
+        self.interfaces = [iface for iface in stanzas if isinstance(iface, Iface)]
+        for i in self.interfaces:
+            stanzas.remove(i)
+
+        self.mappings = [mapping for mapping in stanzas if isinstance(mapping, Mapping)]
+        for i in self.mappings:
+            stanzas.remove(i)
+
+        self.sources = [source for source in stanzas if isinstance(source, (Source, SourceDirectory))]
+        subfiles = []
+        for i in self.sources:
+            if isinstance(i, SourceDirectory):
+                d = self.normalize_path(i.source_directory)
+                subfiles += [os.path.join(d, f) for f in os.listdir(d)
+                             if os.path.isfile(os.path.join(d, f)) and
+                             re.match('^[a-zA-Z0-9_-]+$', f)]
+            else:
+                subfiles.append(self.normalize_path(i.source_filename))
+
+        for subfile in subfiles:
+            self.sub_files.append(InterfacesFile(subfile))
+
+        for startup in [s for s in stanzas if isinstance(s, StartupStanza)]:
+            self.get_iface(startup.iface_name).startup = startup
+
+    def find_iface(self, name):
+        return [iface for iface in self.interfaces if iface.name.index(name)]
+
+    def get_iface(self, name):
+        result = [iface for iface in self.interfaces if iface.name == name]
+        if result:
+            return result[0]
+
+        for s in self.sub_files:
+            try:
+                return s.get_iface(name)
+            except KeyError:
+                continue
+
+        raise KeyError(name)
+
+
+    def normalize_path(self, p):
+        if p.startswith('/'):
+            return p
+        return os.path.join(self.dirname, p)
---
+++ tests/test_interfaces.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import unittest
+from os.path import join, dirname, abspath
+from network_interfaces import InterfacesFile
+__author__ = 'vahid'
+
+this_dir = abspath(dirname(__file__))
+data_dir = join(this_dir, 'data')
+
+class NetworkingCase(unittest.TestCase):
+
+    def setUp(self):
+        self.interfaces_filename = join(data_dir, 'interfaces')
+
+    def test_interfaces(self):
+        f = InterfacesFile(self.interfaces_filename)
+
+        self.assertRaises(KeyError, f.get_iface, 'non-existance-iface')
+
+        lo = f.get_iface('lo')
+        self.assertEquals(lo.startup.mode, 'auto')
+        self.assertEquals(lo.name, 'lo')
+        self.assertEquals(lo.address_family, 'inet')
+        self.assertEquals(lo.method, 'loopback')
+
+        eth0 = f.get_iface('eth0')
+        self.assertEquals(eth0.startup.mode, 'auto')
+        self.assertEquals(eth0.name, 'eth0')
+        self.assertEquals(eth0.address_family, 'inet')
+        self.assertEquals(eth0.method, 'static')
+        self.assertEquals(eth0.address, '192.168.11.2')
+        self.assertEquals(eth0.netmask, '255.255.255.240')
+        self.assertEquals(eth0.broadcast, '192.168.11.15')
+        self.assertEquals(eth0.network, '192.168.11.0')
+        self.assertEquals(eth0.gateway, '192.168.11.1')
+        self.assertEquals(eth0.dns_nameservers, '8.8.8.8 8.8.4.4')
+
+        wlan0 = f.get_iface('wlan0')
+        self.assertEquals(wlan0.startup.mode, 'allow-hotplug')
+        self.assertEquals(wlan0.name, 'wlan0')
+        self.assertEquals(wlan0.address_family, 'inet')
+        self.assertEquals(wlan0.method, 'static')
+        self.assertEquals(wlan0.address, '192.168.9.41')
+        self.assertEquals(wlan0.netmask, '255.255.255.0')
+        self.assertEquals(wlan0.broadcast, '192.168.9.255')
+        self.assertEquals(wlan0.network, '192.168.9.0')
+        self.assertEquals(wlan0.gateway, '192.168.9.1')
+        self.assertEquals(wlan0.wpa_ssid, 'khayyam')
+        self.assertEquals(wlan0.wpa_psk, '<-Wine&Is&Mine->')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
