776aca26a56ff35e83648459e63187f2dfa4c6ba
pylover <vahid.mardani@gmail.com>
2015-06-08 00:33:58 +0430

    some design enhancements

--- network_interfaces/stanza.py
+++ network_interfaces/stanza.py
@@ -7,12 +7,10 @@ class Stanza(object):
     _type = None
     _filename = None
     _headers = None
-    _items = None
 
     def __init__(self, filename, *headers):
         self._filename = filename
         self._headers = headers
-        self._items = []
 
     def add_entry(self, l):
         cells = re.split('\s+', l)
@@ -22,66 +20,6 @@ class Stanza(object):
 
     def __repr__(self):
         return ' '.join(self._headers)
-
-    def __getattr__(self, item):
-        try:
-            return self[item]
-        except (KeyError, IndexError):
-            raise AttributeError('%s %s' % (object.__repr__(self), item))
-
-    def __setattr__(self, key, value):
-        if hasattr(self.__class__, key):
-            super(Stanza, self).__setattr__(key, value)
-        else:
-            self[key] = value
-
-    def __delattr__(self, item):
-        if hasattr(self.__class__, item):
-            super(Stanza, self).__delattr__(item)
-        else:
-            del self[item]
-
-    def __getitem_internal(self, item):
-        key = item.replace('_', '-')
-        for i in self._items:
-            if i[0] == key:
-                return i
-        return None
-
-    def __delitem_internal(self, item):
-        key = item.replace('_', '-')
-        for i in self._items:
-            if i[0] == key:
-                self._items.remove(i)
-                return
-
-    def __contains__(self, item):
-        return self.__getitem_internal(item) is not None
-
-    def __getitem__(self, item):
-        if not isinstance(item, basestring):
-            raise TypeError(type(item))
-        result = self.__getitem_internal(item)
-        if not result:
-            raise KeyError(item)
-        return ' '.join(result[1:])
-
-    def __setitem__(self, key, value):
-        if not isinstance(key, basestring):
-            raise TypeError(type(key))
-        values = re.split('\s', value)
-
-        cells = self.__getitem_internal(key)
-        if not cells:
-            self.add_entry(' '.join([key] + values))
-        else:
-            del cells[1:]
-            cells += values
-
-    def __delitem__(self, item):
-        if not isinstance(item, basestring):
-            raise TypeError(type(item))
-        self.__delitem_internal(item)
 
     def _items_hash(self):
         result = 0
@@ -98,8 +36,7 @@ class Stanza(object):
     def __hash__(self):
         return \
             self._type.__hash__() ^ \
-            self._headers_hash() ^ \
-            self._items_hash()
+            self._headers_hash()
 
     @classmethod
     def is_stanza(cls, s):
@@ -129,9 +66,82 @@ class Stanza(object):
 
 
 class MultilineStanza(Stanza):
+    _items = None
+
+    def __init__(self, *args, **kwargs):
+        super(MultilineStanza, self).__init__(*args, **kwargs)
+        self._items = []
+
+    def __getattr__(self, item):
+        try:
+            return self[item]
+        except (KeyError, IndexError):
+            raise AttributeError('%s %s' % (object.__repr__(self), item))
+
+    def __setattr__(self, key, value):
+        if hasattr(self.__class__, key):
+            super(Stanza, self).__setattr__(key, value)
+        else:
+            self[key] = value
+
+    def __delattr__(self, item):
+        if hasattr(self.__class__, item):
+            super(Stanza, self).__delattr__(item)
+        else:
+            del self[item]
+
+    def __contains__(self, item):
+        return self.__getitem_internal(item) is not None
+
+    def __getitem__(self, item):
+        if not isinstance(item, basestring):
+            raise TypeError(type(item))
+        result = self.__getitem_internal(item)
+        if not result:
+            raise KeyError(item)
+        return ' '.join(result[1:])
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, basestring):
+            raise TypeError(type(key))
+        values = re.split('\s', value)
+
+        cells = self.__getitem_internal(key)
+        if not cells:
+            self.add_entry(' '.join([key] + values))
+        else:
+            del cells[1:]
+            cells += values
+
+    def __delitem__(self, item):
+        if not isinstance(item, basestring):
+            raise TypeError(type(item))
+        self.__delitem_internal(item)
+
     def __repr__(self):
         return '%s\n%s\n' % (
             super(MultilineStanza, self).__repr__(),
             '\n'.join(['  ' + ' '.join(i) for i in self._items]))
 
+    def __hash__(self):
+        return super(MultilineStanza, self).__hash__() ^ self._items_hash()
 
+    def _items_hash(self):
+        result = 0
+        for i in self._items:
+            result ^= list_hash(i)
+        return result
+
+    def __getitem_internal(self, item):
+        key = item.replace('_', '-')
+        for i in self._items:
+            if i[0] == key:
+                return i
+        return None
+
+    def __delitem_internal(self, item):
+        key = item.replace('_', '-')
+        for i in self._items:
+            if i[0] == key:
+                self._items.remove(i)
+                return
